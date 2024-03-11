2cf7fdaae2c5ca68267405744996493700bf789b
abdullah <abdullahwebwiz@gmail.com>
2024-03-11 17:53:35 +0500

    f

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,15 +1,19 @@
 "use client";
 import MyImage from "@/components/myimage/myimage";
+import { useState } from "react";
 export default function Page() {
+  let [download, setdownload] = useState(false);
   return (
     <div>
-      <div>About</div>
+      <div onClick={() => setdownload(true)}>About</div>
       <MyImage
         folder={"productimages"}
         name={"5nyjdxaheb0" + ".png"}
         width={200}
         height={200}
         alt={"my img"}
+        download={download}
+        downname={'1'}
       />
     </div>
   );
