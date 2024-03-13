7728e1455a0e4011cd49c5b18348616fc80e452c
abdullah <abdullahwebwiz@gmail.com>
2024-03-13 18:13:51 +0500

    try10

--- src/data/data2.js
+++ src/data/data2.js
@@ -1,5 +1,5 @@
 export const data2 = {
-  development: "http://localhost:3000",
+  // development: "http://localhost:3000",
   // production:'http://localhost:3000',
   production: "https://mydropshipsite.vercel.app",
 };
