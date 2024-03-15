bc10c225c290dfcfce65182d8e2b24bfd8a3588b
abdullah <abdullahwebwiz@gmail.com>
2024-03-15 06:49:15 +0500

    all done haaha

--- src/data/data2.js
+++ src/data/data2.js
@@ -1,5 +1,5 @@
 export const data2 = {
-  production:'http://localhost:3000',
-  // production: "https://mydropshipsite.vercel.app",
+  // production:'http://localhost:3000',
+  production: "https://mydropshipsite.vercel.app",
   fileurl:'https://zlqi1znpfydrhxa5.public.blob.vercel-storage.com/'
 };
